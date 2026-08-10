f576d834ab0be322201de62098d460780e9d2503
Victorien <65306057+Viicos@users.noreply.github.com>
2026-08-10 11:10:35 +0200

    Fix `Literal` test (#56)
    https://github.com/python/cpython/pull/153914 deduplicates unhashable arguments.

--- tests/introspection/test_literal_values.py
+++ tests/introspection/test_literal_values.py
@@ -76,4 +76,12 @@ def test_literal_values_unpack_type_aliases_undefined(create_module) -> None:
 
 
 def test_literal_values_unhashable_type() -> None:
+    assert list(get_literal_values(t_e.Literal[[1, 'a']])) == [[1, 'a']]
+
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 13, 15),
+    reason='Unashable arguments are deduplicated (https://github.com/python/cpython/pull/153914)',
+)
+def test_duplicate_literal_values_unhashable_type() -> None:
     assert list(get_literal_values(t_e.Literal[[1, 'a'], [1, 'a']])) == [[1, 'a'], [1, 'a']]  # noqa: PYI062
